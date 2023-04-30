cb4f0adb1ed2d554e46d846c3a1adf0f241554de
George Leon <george.leon.external@atos.net>
2023-04-30 14:40:22 +0300

    Revert "logging fixed"
    This reverts commit 35c5be574cdf3b6838e9529541684dd097f9fc84.

--- src/main/java/com/leonsoft/app/config/LoggingInterceptor.java
+++ src/main/java/com/leonsoft/app/config/LoggingInterceptor.java
@@ -3,10 +3,12 @@ package com.leonsoft.app.config;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
-
 @Slf4j
 public class LoggingInterceptor implements HandlerInterceptor {
+
 
 
     @Override
@@ -19,19 +21,6 @@ public class LoggingInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         // Log response details
-        log.info("Sent response: {} {} with status {}", request.getMethod(), request.getRequestURI(), response.getStatus());
-        if ( ex != null ) {
-
-            log.error("-BEGIN-ERROR");
-
-            log.error("ERROR:::>>>>\n{}",ex.getMessage());
-
-
-
-            ex.fillInStackTrace();
-            log.error("-END-ERROR");
-        }
-
-
+        log.info("Sent response: {} {} with status {} and exception {}", request.getMethod(), request.getRequestURI(), response.getStatus(), ex);
     }
 }
